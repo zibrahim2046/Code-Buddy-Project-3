32498f953c3cff23ce6fa9efce0d57c9b81dd457
Ian <ian@macbook-pro.lan>
2020-06-13 13:22:35 -0500

    Updates

---
+++ precache-manifest.cf68fe0a4af785514784d3921d7670f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9d8302151c5c28bac30fa84055c07a",
+    "url": "/code-buddy-project-3/index.html"
+  },
+  {
+    "revision": "4fe4c3383019f9fbb6e0",
+    "url": "/code-buddy-project-3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76ef70db5facff0ffe83",
+    "url": "/code-buddy-project-3/static/js/2.a75d9476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-buddy-project-3/static/js/2.a75d9476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe4c3383019f9fbb6e0",
+    "url": "/code-buddy-project-3/static/js/main.6bd23502.chunk.js"
+  },
+  {
+    "revision": "c6716353c3afdbb7217a",
+    "url": "/code-buddy-project-3/static/js/runtime-main.a32f96d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code-buddy-project-3/static/media/logo.5d5d9eef.svg"
+  }
+]);
