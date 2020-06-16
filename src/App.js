b07b371e342c5b45344eb64cdb89d3cd0bc04fa9
Ian <ian@macbook-pro.lan>
2020-06-15 22:40:24 -0500

    made landing page with input for username/password

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
 const App = () => {
@@ -7,6 +8,21 @@ const App = () => {
         <div className='App'>
             <h1>welcome to code buddy</h1>
             <p>please sign in to continue</p>
+            <Form inline>
+                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                    <Label for='exampleEmail' className='mr-sm-2'>
+                        username
+                    </Label>
+                    <Input type='username' name='username' id='username' placeholder='' />
+                </FormGroup>
+                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
+                    <Label for='password' className='mr-sm-2'>
+                        password
+                    </Label>
+                    <Input type='password' name='password' id='password' placeholder='' />
+                </FormGroup>
+                <Button>Submit</Button>
+            </Form>
         </div>
     );
 };
