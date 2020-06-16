d9f232c00e0e81d8f7f8e2d48ac2a7a18cd22883
Ian <ian@macbook-pro.lan>
2020-06-16 01:00:07 -0500

    formatted templates for main page, help page, loading page, results page, and 404 page. Set up button dropdowns and buttons. Used React Router to navigate through the various pages and reactstrap for basic styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
-const App = () => {
-    return (
-        <div className='App'>
-            <h1>welcome to code buddy</h1>
-            <p>please sign in to continue</p>
-            <Form inline>
-                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                    <Label for='exampleEmail' className='mr-sm-2'>
-                        username
-                    </Label>
-                    <Input type='username' name='username' id='username' placeholder='' />
-                </FormGroup>
-                <FormGroup className='mb-2 mr-sm-2 mb-sm-0'>
-                    <Label for='password' className='mr-sm-2'>
-                        password
-                    </Label>
-                    <Input type='password' name='password' id='password' placeholder='' />
-                </FormGroup>
-                <Button>Submit</Button>
-            </Form>
-        </div>
-    );
-};
+//Pages
+import MainPage from './pages';
+import NotFound from './pages/404';
+import HelpPage from './pages/helpPage';
+import LoadingPage from './pages/loadingPage';
+import Results from './pages/results';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={MainPage} />
+                    <Route exact path='/help' component={HelpPage} />
+                    <Route exact path='/load' component={LoadingPage} />
+                    <Route exact path='/results' component={Results} />
+                    <Route exact path='/404' component={NotFound} />
+                    <Redirect to='/404' />
+                </Switch>
+            </Router>
+        );
+    }
+}
 export default App;
---
+++ src/pages/loadingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <h1>okay! let me see what i can find...</h1>
+        </div>
+    );
+};
+export default LoadingPage;
