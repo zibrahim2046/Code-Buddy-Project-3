61220c0938578bc8302eb3cfa4b67b818a882bcd
Ian <ian@macbook-pro.lan>
2020-06-16 01:13:02 -0500

    added spinner gif to loading page

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../assets/images/spinner.gif';
 
 const LoadingPage = () => {
     return (
         <div>
             <h1>okay! let me see what i can find...</h1>
+            <img src={logo} alt='loading...' />
         </div>
     );
 };
