f8aad218deae66e723b1cd5693d0fa6d55e0b2b3
Ian <ian@macbook-pro.lan>
2020-06-25 20:54:21 -0500

    added components for save, refresh, and delete buttons

---
+++ client/src/components/RefreshBtn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const RefreshBtn = () => {
+    return <Button color='secondary'>Refresh</Button>;
+};
+
+export default RefreshBtn;
