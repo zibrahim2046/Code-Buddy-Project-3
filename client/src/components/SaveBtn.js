3e4dc4b743ab363a652a8b2620990060b8ebaeb6
Ian <ian@macbook-pro.lan>
2020-07-05 20:48:19 -0500

    changes

--- client/src/components/SaveBtn.js
+++ client/src/components/SaveBtn.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 const SaveBtn = () => {
-    return <Button color='secondary'>Save</Button>;
+    return (
+        <Button onClick={() => this.postData()} color='secondary'>
+            Save
+        </Button>
+    );
 };
 
 export default SaveBtn;
