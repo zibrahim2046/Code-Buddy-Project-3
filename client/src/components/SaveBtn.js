be4df318a8c459842687a1ef4a93670390eb8c77
jadibrahim99 <jadintx@gmail.com>
2020-07-05 20:52:38 -0500

    api routing test

--- client/src/components/SaveBtn.js
+++ client/src/components/SaveBtn.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
-const SaveBtn = () => {
-    return <Button color='secondary'>Save</Button>;
-};
+
+class SaveBtn extends Component {
+    async postData() {
+        try {
+            let result = await fetch('https://webhook.site/4cb72455-603c-44a4-b29b-d93a3755caf6', {
+                method: 'post',
+                mode: 'no-cors',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    videoID: 'kLO4X_3VYdg'
+                })
+            });
+            console.log('Result:' + result)
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+    render() {
+
+        return (<Button onClick={() => this.postData()} color='secondary'>Save</Button>
+        )
+    };
+
+
+
+
+
+
+
+
+}
+
+
 
 export default SaveBtn;
