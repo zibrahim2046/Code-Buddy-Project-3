b14d436f9654d035ca8880ecb31e4d916f087b8d
Ian <ian@macbook-pro.lan>
2020-07-08 12:41:54 -0500

    set up context for user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
 import React, { useEffect, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/authActions';
-// import NavBar from './components/NavBar'
-
-// import './App.css';
-
-//Pages
 import MainPage from './pages/Index';
 import NotFound from './pages/404';
 import HelpPage from './pages/HelpPage';
 import Results from './pages/Results';
 import SavedPage from './pages/Saved';
-
 import Layout from './layout/Layout';
+import VideoContextProvider from '../src/context/VideoContext';
 
 class App extends Component {
     componentDidMount() {
@@ -25,47 +19,22 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Layout>
-                    <Router>
-                        <Switch>
-                            <Route exact path='/' component={MainPage} />
-                            <Route exact path='/help' component={HelpPage} />
-                            <Route exact path='/results' component={Results} />
-                            <Route exact path='/saved' component={SavedPage} />
-                            <Route exact path='/404' component={NotFound} />
-                            <Redirect to='/404' />
-                        </Switch>
-                    </Router>
-                </Layout>
+                <VideoContextProvider>
+                    <Layout>
+                        <Router>
+                            <Switch>
+                                <Route exact path='/' component={MainPage} />
+                                <Route exact path='/help' component={HelpPage} />
+                                <Route exact path='/results' component={Results} />
+                                <Route exact path='/saved' component={SavedPage} />
+                                <Route exact path='/404' component={NotFound} />
+                                <Redirect to='/404' />
+                            </Switch>
+                        </Router>
+                    </Layout>
+                </VideoContextProvider>
             </Provider>
         );
     }
 }
 export default App;
-
-// const App = () => {
-//     useEffect(() => {
-//         store.dispatch(loadUser());
-//     }, []);
-
-//     return (
-//         <Provider store={store}>
-//             {/* <React.Fragment> */}
-//                 {/* <NavBar /> */}
-//                 <Layout>
-//                     <Router>
-//                         <Switch>
-//                             <Route exact path='/' component={MainPage} />
-//                             <Route exact path='/help' component={HelpPage} />
-//                             <Route exact path='/results' component={Results} />
-//                             <Route exact path='/404' component={NotFound} />
-//                             <Redirect to='/404' />
-//                         </Switch>
-//                     </Router>
-//                 </Layout>
-//             {/* </React.Fragment> */}
-//         </Provider>
-//     );
-// }
-
-// export default App;
--- client/src/context/VideoContext.js
+++ client/src/context/VideoContext.js
@@ -1,73 +1,13 @@
-import React, { Component, createContext } from 'react';
-
-
+import React, { createContext, useState } from 'react';
 
 export const VideoContext = createContext();
-class VideoContextProvider extends Component {
-    state = { videoID=[
-        htmlCss = [
-            {
-                title: 'Intro to HTML/CSS',
-                topic: 'introToHTML',
-            },
-            {
-                title: 'Styling your HTML',
-                topic: 'stylingHTML',
-            },
-            {
-                title: 'Advanced HTML/CSS',
-                topic: 'advancedHTML',
-            },
-        ],
-        node = [
-            {
-                title: 'Intro to NodeJS',
-                topic: 'introNodeJS',
-            },
-            {
-                title: 'NodeJS and MYSQL',
-                topic: 'nodeJSmySQL',
-            },
-            {
-                title: 'Restful API',
-                topic: 'restfulAPI',
-            },
-        ],
-        js = [
-            {
-                title: 'Intro to Javascript',
-                topic: 'introJS',
-            },
-            {
-                title: 'For Loops and Functions',
-                topic: 'forLoopsAndFunctions',
-            },
-            {
-                title: 'Back End Javascript',
-                topic: 'backEndJavascript',
-            },
-        ],
-        react = [
-            {
-                title: 'Create-React-App',
-                topic: 'createReactApp',
-            },
-            {
-                title: 'ES6 Syntax',
-                topic: 'es6Syntax',
-            },
-            {
-                title: 'Conditional Rendering',
-                topic: 'conditionalRendering',
-            },
-        ]
-    ]}
-    render() {
-        return( 
-            <VideoContext.Provider value={{...this.state}}>
-                { this.props.children}
-            </VideoContext.Provider>
-        )}
-}
+
+const VideoContextProvider = ({ children }) => {
+    const [user, setUser] = useState({
+        videoID: null,
+    });
+
+    return <VideoContext.Provider value={(user, setUser)}>{children}</VideoContext.Provider>;
+};
 
 export default VideoContextProvider;
