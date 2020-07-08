a23c5ff8ecaf1f4891d160ebdc8416c85d0e95b6
Ian <ian@macbook-pro.lan>
2020-07-08 17:11:13 -0500

    working on file structure

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
 import InitialPage from '../components/InitialPage';
-
 const MainPage = () => {
     return <InitialPage />;
 };
-
 export default MainPage;
