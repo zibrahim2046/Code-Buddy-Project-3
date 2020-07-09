335e5dd16396c87449abc18bac2309a588781add
Ian <ian@macbook-pro.lan>
2020-07-09 18:05:57 -0500

    changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '/client/build')));
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    // app.use(express.static('client/build'));
 }
 
 app.use(routes);
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI || db, {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, +'/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.listen(PORT, function () {
