d53240b97e57ae248ad09eb7e2895877e662acfb
Ian <ian@macbook-pro.lan>
2020-07-09 15:43:40 -0500

    test theory

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.MONGODB_URI || db, {
 // Send every request to the React app
 // Define any API routes before this runs
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, +'/client/build/index.html'));
 });
 
 app.listen(PORT, function () {
