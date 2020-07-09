de675249ecd4f9ab94dcf8839068d83ab5a7cce0
Ian <ian@macbook-pro.lan>
2020-07-09 14:24:25 -0500

    changes

---
+++ client/build/precache-manifest.9a7636ab65e06102e731f9519726637a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fd434cd1a039cd121e0",
+    "url": "/static/css/main.f9399404.chunk.css"
+  },
+  {
+    "revision": "9fd434cd1a039cd121e0",
+    "url": "/static/js/main.4e8a4c3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3300f886049b64b790de",
+    "url": "/static/js/2.bfd1d6cd.chunk.js"
+  },
+  {
+    "revision": "e7e0cb022d70f76f3f369a579aa0484f",
+    "url": "/index.html"
+  }
+];
